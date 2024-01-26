db8be5e29913f38aab48631cea28ab429fd2f9b4
Jnanavi7 <jnanavi19@gmail.com>
2024-01-26 22:26:59 +0530

    Added all components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HelpdeskComponent } from './helpdesk/helpdesk.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { ContactgroupsComponent } from './contactgroups/contactgroups.component';
+import { HeaderComponent } from './header/header.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'helpdesk',component:HelpdeskComponent},
+  {path:'contacts',component:ContactsComponent},
+  {path:'contactgroups/:group',component:ContactgroupsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
